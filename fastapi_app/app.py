ac8429aaf2f9b335dcc37420f19c43143585cf5e
Hassan Zein <Hassan.Zein@lumen.com>
2024-07-10 12:58:32 -0700

    added api route

--- fastapi_app/app.py
+++ fastapi_app/app.py
@@ -2,7 +2,11 @@ from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
 import pickle
+
+class Email(BaseModel):
+    text: str = None
 
 tokenizer = pickle.load(open("models/cv.pkl", "rb"))
 model = pickle.load(open("models/clf.pkl", "rb"))
@@ -20,8 +24,15 @@ def home(request: Request):
 
 @app.post("/predict")
 def predict(request: Request, email_text: str = Form(default="")):
-    print(f"EMAIL TEXT: {[email_text]}")
     tokenized_email = tokenizer.transform([email_text])
     predictions = model.predict(tokenized_email)
     predictions = 1 if predictions == 1 else -1
     return templates.TemplateResponse('index.html', context={'request': request, 'email_text': email_text, 'predictions': predictions})
+
+@app.post("/api/predict")
+def api_predict(email: Email):
+    email_text = email.text
+    tokenized_email = tokenizer.transform([email_text])
+    predictions = model.predict(tokenized_email)
+    predictions = 1 if predictions == 1 else -1
+    return {"predictions": predictions}
